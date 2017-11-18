63db799b4dca0dfabeeacf78ea3124683b76d0c7
Sergey Yarmolenko <iserj@iserj-osx1.local>
2017-11-18 18:25:50 +0300

    add telegramm-bot

---
+++ bot.py
@@ -0,0 +1,29 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import logging
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def greet_user(bot, update):
+    text = 'Вызван /start'
+    print(text)
+    update.message.reply_text(text)
+
+def talk_to_me(bot, update):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+
+def main():
+    updater = Updater("453680228:AAHKfirSvEenS5paUgKAMtGsn1wfNNlkRuk")
+
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    updater.start_polling()
+    updater.idle()
+
+main()
